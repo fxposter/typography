a94d335c178dae85cd0df284691b3a2b799235dd
Pavel Forkert <fxposter@gmail.com>
2011-03-19 18:04:17 +0200

    added to_s in ty() helper

--- lib/typography/helper.rb
+++ lib/typography/helper.rb
@@ -3,7 +3,7 @@ require 'action_view'
 
 module ActionView::Helpers::TextHelper
   def ty(text, options = {})
-    TypographyHelper::Core.new(text.html_safe, options).typography
+    TypographyHelper::Core.new(text.to_s.html_safe, options).typography
   end
   def ty_simple(text, html_options={})
     simple_format ty(text), html_options
